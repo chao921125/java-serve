abccca186ac7f68bb03102912d363624cbfd58bf
hc precog <chao921125@sina.com>
2023-09-17 20:49:48 +0800

    add

--- src/main/java/com/cc/java/JavaServeApplication.java
+++ src/main/java/com/cc/java/JavaServeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaApplication {
+public class JavaServeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaApplication.class, args);
+        SpringApplication.run(JavaServeApplication.class, args);
     }
 
 }
