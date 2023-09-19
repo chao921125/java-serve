f0e2b1ea718e346727bec3b1a1c027e6f0dc77b2
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-09-19 18:04:23 +0800

    add

---
+++ src/main/java/com/cc/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,16 @@
+package com.cc.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import com.cc.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ *
+ * @author ruoyi
+ */
+public class QuartzJobExecution extends AbstractQuartzJob {
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
--- src/test/java/common/utils/ServletUtils.java
+++
@@ -1,4 +0,0 @@
-package common.utils;
-
-public class ServletUtils {
-}
