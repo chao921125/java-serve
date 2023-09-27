f50301a44fc56fb23cc1b57245c8604f4f045b8c
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-09-27 14:24:42 +0800

    add

--- src/main/java/com/cc/generator/RunGen.java
+++ src/main/java/com/cc/generator/RunGen.java
@@ -6,7 +6,9 @@ import com.baomidou.mybatisplus.generator.config.OutputFile;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 public class RunGen {
@@ -14,26 +16,29 @@ public class RunGen {
         final String DATA_SOURCE = "jdbc:mysql://localhost:3306/cc?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8";
         final String DATA_NAME = "root";
         final String DATA_PWD = "root123456";
-        final String PKG = "com.cc.javaserve";
 
-        final String TABLE_NAME = "sys_user";
+        final String TABLE_NAME = "sys_department,sys_menu";
         final String TABLE_NAME_PREFIX = "";
+
+        final String PKG = "system";
+        final String PKG_PATH = "/src/main/java/com/cc/generator";
+        final String PKG_PATH_MAPPER = "/src/main/java/com/cc/generator/mapper";
 
         FastAutoGenerator.create(DATA_SOURCE, DATA_NAME, DATA_PWD)
                 .globalConfig(builder -> {
                     builder.enableSwagger()
                             .author("cc") // 作者
                             .commentDate("yyyy-mm-dd HH:MM:SS") // 时间
-                            .outputDir(System.getProperty("user.dir") + "/src/main/java/com/cc/generator") // 目录
+                            .outputDir(System.getProperty("user.dir") + PKG_PATH) // 目录
                             .disableOpenDir();// 禁止打开目录
                 })
                 .dataSourceConfig(builder -> {})
                 .packageConfig(builder -> {
                     builder.parent(PKG) // 设置父包，mapper路径
-                            .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + "/src/main/java/com/cc/generator/mapper"));
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + PKG_PATH_MAPPER));
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude(TABLE_NAME) // 表
+                    builder.addInclude(getTables(TABLE_NAME)) // 表
                             .addTablePrefix(TABLE_NAME_PREFIX) // 过滤表前缀
                             .entityBuilder() // Entity策略
                             .enableLombok() //
@@ -50,5 +55,10 @@ public class RunGen {
                             .enableFileOverride(); // 覆盖
                 })
                 .execute();
+    }
+
+    // 处理 all 情况
+    protected static List<String> getTables(String tables) {
+        return "all".equals(tables) ? Collections.emptyList() : Arrays.asList(tables.split(","));
     }
 }
--- src/main/java/com/cc/generator/RunGenInput.java
+++ src/main/java/com/cc/generator/RunGenInput.java
@@ -15,23 +15,26 @@ public class RunGenInput {
         final String DATA_SOURCE = "jdbc:mysql://localhost:3306/cc?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8";
         final String DATA_NAME = "root";
         final String DATA_PWD = "root123456";
-        final String PKG = "com.cc.javaserve";
 
         final String TABLE_NAME = "sys_user";
         final String TABLE_NAME_PREFIX = "";
+
+        final String PKG = "system";
+        final String PKG_PATH = "/src/main/java/com/cc/generator";
+        final String PKG_PATH_MAPPER = "/src/main/java/com/cc/generator/mapper";
 
         FastAutoGenerator.create(DATA_SOURCE, DATA_NAME, DATA_PWD)
                 .globalConfig((scanner, builder) -> {
                     builder.enableSwagger()
                             .author(scanner.apply("请输入作者：")) // 作者
                             .commentDate("yyyy-mm-dd HH:MM:SS") // 时间
-                            .outputDir(System.getProperty("user.dir") + "/src/main/java/com/cc/generator") // 目录
+                            .outputDir(System.getProperty("user.dir") + PKG_PATH) // 目录
                             .disableOpenDir();// 禁止打开目录
                 })
                 .dataSourceConfig(builder -> {})
                 .packageConfig(builder -> {
                     builder.parent(PKG) // 设置父包，mapper路径
-                            .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + "/src/main/java/com/cc/generator/mapper"));
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + PKG_PATH_MAPPER));
                 })
                 .strategyConfig((scanner, builder) -> {
                     builder.addInclude(getTables(scanner.apply("请输入表名，多个英文逗号分隔？所有输入 all"))) // 表
