31ab61391090eb7fcbe4e183f6489965b829c997
cc <chao921125@sina.com>
2024-04-26 09:30:19 +0800

    Update run.sh

--- Doc/run.sh
+++ Doc/run.sh
@@ -20,8 +20,13 @@ else
 fi
 
 [ ! -d "$logDir" ] && mkdir -p "$logDir"
-[ ! -d "$logDir" ] && touch "nohup`date +%Y-%m-%d`.out"
-nohup java -jar $jarName --server.port=8080 >> ./logs/nohup`date +%Y-%m-%d`.out 2>&1 &
-echo Server Starting
+[ ! -d "$logDir" ] && touch "nohup.out"
+#[ ! -d "$logDir" ] && touch "nohup`date +%Y-%m-%d`.out"
 #指定输出到当前目录log文件夹中 netstat -anp | grep 8080
-#nohup java -Xms1024m -Xmx2048m -jar ruoyi-admin.jar --server.port=8080 1>start.out 2>&1 &
+#只输出错误日志
+nohup java -jar $jarName --server.port=8080 >> ./logs/nohup.out 2 &
+#错误日志重定向到标准日志
+#nohup java -jar $jarName --server.port=8080 >> ./logs/nohup`date +%Y-%m-%d`.out 2>&1 &
+#重命名日志文件
+#nohup java -Xms1024m -Xmx2048m -jar ruoyi-admin.jar --server.port=8080 /path/log>start.out 2>&1 &
+echo Server Starting
