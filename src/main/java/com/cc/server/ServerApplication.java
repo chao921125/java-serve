5d761e9e92f358aa03b3e7cd02cccb0a5e5d92a1
hd-huangchao <chao.huang@advokategroup.com>
2025-01-06 14:33:43 +0800

    ed file

--- src/main/java/com/cc/server/ServerApplication.java
+++ src/main/java/com/cc/server/ServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+//@SpringBootApplication
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.cc.server.mapper")
 public class ServerApplication {
